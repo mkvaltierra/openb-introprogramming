840a9d2e03551ea547287c1b1a28e9e68162aed2
mkvaltierra <mkvaltierra.web@gmail.com>
2022-03-06 20:10:39 +0100

    first initial-prog ejercicio01-02

---
+++ Ejercicio01-02/src/com/example/Ejercicio01_02.java
@@ -0,0 +1,28 @@
+package com.example;
+
+
+/* Para este ejercicio deberás utilizar como lenguaje Java y hacer lo siguiente:
+
+        - Declarar una variable de tipo int llamada numero1.
+        - Mostrarla por pantalla.
+        - Modificar el valor de la variable numero1.
+        - Mostrarla por pantalla.
+        - Crea un array de 4 elementos.
+        - Mostrar el elemento en la posición 0.
+*/
+
+public class Ejercicio01_02 {
+
+    public static void main(String[] args){
+
+        int numero1 = 5;
+        System.out.println(numero1);
+        numero1 = 10;
+        System.out.println(numero1);
+
+        String [] names = {"Martin", "Margot", "Marious", "Maika"};
+        System.out.println(names[0]);
+
+
+    }
+}
